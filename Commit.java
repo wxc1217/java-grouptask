1324a595a0b1b754038010b2d9af38093f026a7a
RitaPeng0101 <73017416+RitaPeng0101@users.noreply.github.com>
2020-12-15 19:53:17 +0800

    Update Commit.java

--- Commit.java
+++ Commit.java
@@ -6,9 +6,8 @@ public class Commit extends Hash {
     private String value;
     private String presentcommit;
 
-    
     public Commit(String rootDirPath) throws Exception {
-        String rootDirKey=Hash.Treehash(rootDirPath)
+        String rootDirKey=Hash.prKVstore(rootDirPath)  //根据所给定工作区目录，对根文件夹进行遍历并返回对应key值；
         File HEAD=new File(Hash.objectpath + File.separator + "HEAD");
 
         if(HEAD.exists()) {
