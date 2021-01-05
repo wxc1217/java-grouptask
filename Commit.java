51d5bf862532eba2938f5cbc44b7154f45fb8545
RitaPeng0101 <73017416+RitaPeng0101@users.noreply.github.com>
2021-01-05 15:47:00 +0800

    Update Commit.java

--- Commit.java
+++ Commit.java
@@ -14,8 +14,8 @@ public class Commit extends Hash{
     public Commit(){}
 
     //仅给定Commit的Key的构造方法
-    public Commit(String key){
-        this.key=key;
+    public Commit(String givenCommitKey){
+        this.key=givenCommitKey;
     }
 
     //给定根目录路径、作者信息、提交者信息、备注信息4个字符串参数的构造方法
@@ -27,19 +27,19 @@ public class Commit extends Hash{
 
         //判定工作目录下是否已有HEAD文件，分情况处理
         if(HEAD.exists()) {
-            String lastRootDirKey=readHEAD();
+            Commit lastCommit=new Commit(readHEAD());
+            String lastRootDirKey=lastCommit.getRootTreeKey();
 
             if(lastRootDirKey==rootDirKey){ //若有HEAD文件且根目录Key与上次Commit中储存的根目录Key相同，则为重复提交，提示用户即可；
                 System.out.println("文件未发生变动，无需commit。" );
             }
             else{//若有HEAD文件且根目录Key不同，读入previousCommit，创建Commit，更新HEAD指针文件；
-                this.previousCommit=lastRootDirKey;
+                this.previousCommit=lastCommit.getKey();
                 createCommit(rootDirKey,author,committer,message);
-                updateHEAD(rootDirKey);
             }    
         }
         else{ //若无HEAD文件，创建并设置previousCommit为空，创建Commit；
-            HEAD.createNewFile();
+            file.createNewFile();
             this.previousCommit="";
             createCommit(rootDirKey,author,committer,message);  
         }
@@ -60,25 +60,38 @@ public class Commit extends Hash{
         this.value += this.message+"\n";
         
         //根据value计算key
-        this.key=Hash.Treehash(this.value);
+        StringBuilder key = new StringBuilder();
+        try {
+            ByteArrayInputStream is = new ByteArrayInputStream(this.value.getBytes());
+            byte[] sha1 = Hash.SHA1Checksum(is);
+            for (int i=0;i<sha1.length;i++) {
+                key.append(Integer.toString((sha1[i]>>4)&0x0F, 16));
+                key.append(Integer.toString(sha1[i]&0x0F, 16));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        this.key = key;
 
-        //根据Key和Value储存为以key为名的Commit文件，写入磁盘
+        //根据Key和Value创建Commit文件写入磁盘
         Hash.mystorage(this.key,this.value);
+
+        //更新HEAD指针
+        updateHEAD(this.key);
     }
 
     //从HEAD指针文件中读取上一次Commit的内容，也即上一次的根目录Key；
     public String readHEAD() throws IOException{
-        File HEAD=new File(Hash.objectpath + File.separator + "HEAD");
         this.previousCommit=Hash.readFromTextFile(HEAD);
         return previousCommit;
     }
 
     //更新HEAD，覆盖写入本次的根目录Key；
-    public void updateHEAD(String rootDirKey){
+    public void updateHEAD(String key){
         try {
             FileWriter writer;
             writer =new FileWriter(Hash.objectpath + File.separator + "HEAD");
-            writer.write(rootDirKey);
+            writer.write(key);
             writer.flush();
             writer.close();
         } catch (IOException e) {
