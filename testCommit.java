0a0be33814f76f041b93ca0444ff9a7cb22ffe31
RitaPeng0101 <73017416+RitaPeng0101@users.noreply.github.com>
2021-01-06 00:53:15 +0800

    Update testCommit.java
    备注部分的首次运行代码 write by张楠 @ZhangNan13

--- testCommit.java
+++ testCommit.java
@@ -1,24 +1,29 @@
-import java.io.IOException;
 import java.util.Scanner;
 
-public class testCommit {
-	public static void main(String args[])throws IOException {
-		
-        Scanner input = new Scanner(System.in);
-        System.out.println("请输入Commit根目录路径：");
-        String rootDirPath= input.next();
-        System.out.println("请输入作者名：");
-        String author= input.next();
-        System.out.println("请输入提交者名：");
-        String committer= input.next();
-        System.out.println("请输入备注信息：");
-        String message= input.next();
-
-        Commit testCommit=new Commit(rootDirPath,author,committer,message);
-		
-        System.out.println("本次Commit的Value为："+testCommit.getValue());
-        System.out.println("本次Commit的Key为："+testCommit.getKey());
-	input.close();
-		
-	}
+public class CommitTest{
+    public static void main(String[] args) {
+        // 首次运行时须运行此部分，避免出错 (此部分by张楠 @ZhangNan13）
+        //Hash.initsetting();
+        //Branch.branchcreate("main","");
+        //Branch.branchchange("main");
+        try {
+            // 提示用户输入文件夹路径名
+            System.out.println("请输入文件夹路径：" );
+            Scanner input = new Scanner(System.in);
+            String rootDirPath = input.next();
+            System.out.println("请输入作者名：");
+            String author= input.next();
+            System.out.println("请输入提交者名：");
+            String committer= input.next();
+            System.out.println("请输入备注信息：");
+            String message= input.next();
+            input.close();
+            Commit testCommit=new Commit(rootDirPath,author,committer,message);
+            testCommit.createCommit(testCommit);
+            System.out.println("本次Commit的Value为："+testCommit.getValue());
+            System.out.println("本次Commit的Key为："+testCommit.getKey());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
