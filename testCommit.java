a2b709a3efc76c6986714d582a7ed5bc19a597ca
RitaPeng0101 <73017416+RitaPeng0101@users.noreply.github.com>
2021-01-06 14:13:27 +0800

    Update testCommit.java

--- testCommit.java
+++ testCommit.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class CommitTest{
     public static void main(String[] args) {
-        // 首次运行时须运行此部分，避免出错 ————此部分by张楠 @ZhangNan13
-        //Hash.initsetting();
-        //Branch.branchcreate("main","");
-        //Branch.branchchange("main");
+        // 首次运行时须运行此部分，避免出错
+        // Hash.initsetting();
+        // Branch.branchcreate("main","");
+        // Branch.branchchange("main");
         try {
             // 提示用户输入文件夹路径名
             System.out.println("请输入文件夹路径：" );
@@ -18,9 +18,9 @@ public class CommitTest{
             System.out.println("请输入备注信息：");
             String message= input.next();
             input.close();
-            Commit testCommit=new Commit(rootDirPath,author,committer,message);
-            testCommit.createCommit(testCommit);
-            System.out.println("本次Commit的Value为："+testCommit.getValue());
+            Commit testCommit = Commit.createCommit(rootDirPath, author, committer, message);
+            System.out.println("本次Commit的Value为：");
+            System.out.println(testCommit.getValue());
             System.out.println("本次Commit的Key为："+testCommit.getKey());
         } catch (Exception e) {
             e.printStackTrace();
